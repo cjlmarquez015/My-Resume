52d89b94d62c42c13583dbe061792b9f52c8a70c
cjlmarquez015 <cjlmarquez.student@ua.edu.ph>
2022-09-17 23:37:26 +0800

    updates done by...

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,101 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
+      <section className={utilStyles.headingMd}>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <p><b>Contact Information:</b></p>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+      <p>Address: 145 Centro Street San Nicolas Mexico Pampanga</p>
+
+      <p>Landline: 8750266</p>
+
+      <p>Cellphone: +639 657 249 719 </p>
+
+      <p>Email Address: cemjecermarquez015@outlook.com </p>
+
+      <p>Facebook Link: <Link href="https://www.facebook.com/cemjecer015/">cemjecer015</Link></p>
+
+      <p><b>Description:</b></p>
+
+      <p>BS CPE Student with Basic knowledge on C#, HTML, CSS, and Javascript</p>
+
+      <p><b>Skills:</b></p>
+      <ul>
+        <li>Basic HTML</li>
+        <li>Basic CSS</li>
+        <li>Basic Javascript</li>
+        <li>Csharp Fundamentals</li>
+      </ul>
+
+      <p><b>Educational Attainment:</b></p>
+      <div>
+        <p style={{textAlign: "left"}}><b>Tertiary Level</b></p>
+        <p style={{textAlign: "left"}}>
+        Bachelor of Science in Computer Engineering
         </p>
+        <p style={{textAlign: "left"}}>
+        University of the Assumption
+        </p>
+        <p style={{textAlign: "left"}}>
+        Unisite Subdivision, Barangay Del Pilar, 
+        </p>
+        <p style={{textAlign: "left"}}>
+        City of San Fernando Pampanga 
+        </p>
+        <p style={{textAlign: "left"}}>
+        2018-Present
+        </p>
+      </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <div>
+        <p style={{textAlign: "left"}}><b>Secondary Level</b></p>
+        <p style={{textAlign: "left"}}>
+        Our Lady of Fatima University
+        </p>
+        <p style={{textAlign: "left"}}>
+        City of San Fernando Pampanga
+        </p>
+        <p style={{textAlign: "left"}}>
+        Senior High School S.Y. 2016-2018 
+        </p>
+        <br></br>
+        <p style={{textAlign: "left"}}>
+        Diosdado Macapagal HighSchool, Inc.
+        </p>
+        <p style={{textAlign: "left"}}>
+        Sto. Domingo Mexico, Pampanga
+        </p>
+        <p style={{textAlign: "left"}}>
+        Junior High School S.Y. 2012-2016
+        </p>
+      </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      <div>
+        <p style={{textAlign: "left"}}><b>Primary Level</b></p>
+        <p style={{textAlign: "left"}}>
+        Mexico Elementary School
+        </p>
+        <p style={{textAlign: "left"}}>
+        Sto. Cristo Mexico, Pampanga
+        </p>
+        <p style={{textAlign: "left"}}>
+        Elementary S.Y. 2006-2012
+        </p>
+      </div>
+      <br></br>
+        <p style={{textAlign: "left"}}>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
 }
